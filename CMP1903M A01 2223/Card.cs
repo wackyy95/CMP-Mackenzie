4754eff0c3ca07fc6c0c621f0cf529b3df77aacb
Mackenzie Wilson <mac.wilson.2004@hotmail.com>
2023-03-07 18:25:46 +0000

    this commit changes limited the amount of cards to 13

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -23,7 +23,7 @@ namespace CMP1903M_A01_2223
 
             set
             {
-                if (_value <= 14)
+                if (_value <= 13)
                 {
                     _value = value + 1;
                 }
